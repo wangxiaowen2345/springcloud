ddbe60df123b49e4ffa50e727ff55f9d7677eacf
wxw <wangxiaowen2345@163.com>
2019-04-23 10:17:22 +0800

    zuul 路由映射 访问地址 localhost：端口+微服务名称（小写）+路径

---
+++ micro-gateway-9527/src/main/java/com/qust/springcloud/ApplicationZuul.java
@@ -0,0 +1,15 @@
+package com.qust.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ApplicationZuul {
+
+
+    public static void main(String [] args) {
+        SpringApplication.run(ApplicationZuul.class ,args);
+    }
+}
